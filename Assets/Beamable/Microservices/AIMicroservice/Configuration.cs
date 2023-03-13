832baaf0d0b8741c039d31fdbd3b8aaadae6f4a8
ben-beamable <71026071+ben-beamable@users.noreply.github.com>
2023-03-13 10:03:04 -0400

    Update Configuration.cs

--- Assets/Beamable/Microservices/AIMicroservice/Configuration.cs
+++ Assets/Beamable/Microservices/AIMicroservice/Configuration.cs
@@ -3,7 +3,7 @@
     internal class Configuration
     {
         // todo put that into realm config and read it from there
-        public const string OpenAiApiKey = "<OpenAI key needed>";
+        public const string OpenAiApiKey = "OpenAI key needed";
         public const string ScenarioApiKey = "Scenario key needed";
     }
 }
